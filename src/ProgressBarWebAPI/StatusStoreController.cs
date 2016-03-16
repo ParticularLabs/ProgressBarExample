387e1b785dd80f29286c17fb085a917d9c2fc930
Colin Higgins <colin.higgins@particular.net>
2016-03-16 12:27:24 -0400

    Add v5 sample

---
+++ src/ProgressBarWebAPI/StatusStoreController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace ProgressBarWebAPI
+{
+    public class StatusStoreController : ApiController
+    {
+        readonly Dictionary<Guid, List<Guid>> _completedItems = new Dictionary<Guid, List<Guid>>();
+
+        [HttpPost]
+        [Route("CompleteMessage")]
+        public void CompleteMessage(CompletedMessage message)
+        {
+            if (!_completedItems.ContainsKey(message.BatchId))
+            {
+                _completedItems.Add(message.BatchId, new List<Guid>());
+            }
+
+            _completedItems[message.BatchId].Add(message.MessageId);
+
+            Console.WriteLine($"Message {message.MessageId} marked as completed.");
+        }
+
+        [HttpGet]
+        [Route("Batch")]
+        public BatchStatus Status(string id)
+        {
+            Console.WriteLine($"Query for status of {id}.");
+
+            var batchId = Guid.Parse(id);
+
+            if (!_completedItems.ContainsKey(batchId))
+            {
+                return null;
+            }
+
+            return new BatchStatus()
+            {
+                Id = batchId,
+                ItemsCompleted = _completedItems[batchId]
+            };
+        }
+    }
+}
---
+++ src/Server/BigStuffHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using NServiceBus;
+
+public class BigStuffHandler : IHandleMessages<TriggerBigStuff>
+{
+    IBus _bus;
+    readonly IStatusStoreClient _statusStoreClient;
+
+    public BigStuffHandler(IBus bus, IStatusStoreClient statusStoreClient)
+    {
+        _bus = bus;
+        _statusStoreClient = statusStoreClient;
+    }
+
+    public void Handle(TriggerBigStuff message)
+    {
+        Console.WriteLine("Handling some big stuff.");
+        for (var index = 0; index <= message.HowMuchStuff; index++)
+        {
+            var messageId = DoSomeWorkForABatch(message, index);
+            _statusStoreClient.AddCompletedCommandToBatch(batchId: message.Id, messageId: messageId);
+        }
+    }
+
+    private Guid DoSomeWorkForABatch(TriggerBigStuff message, int index)
+    {
+        Thread.Sleep(10);
+        Console.WriteLine("Batch Id: " + message.Id + " - Handling message " + index + " of " + message.HowMuchStuff);
+        //Just pretend we did some stuff and sent a message
+        return Guid.NewGuid();
+    }
+}
